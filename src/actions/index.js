fb57cff0f8cb660f2b81d374d14f02b44e12abf5
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-06 14:26:39 +0100

    Set up reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,9 @@
+import axios from 'axios';
+
+export const FETCHING = 'FETCHING';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
+
 // we'll need axios
 
 // we'll need to create 3 different action types here.
@@ -6,3 +12,28 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+
+export const fetching = () => ({
+  type: FETCHING
+});
+
+export const success = characters => ({
+  type: SUCCESS,
+  payload: { characters }
+});
+
+export const failure = error => ({
+  type: FAILURE,
+  payload: { error }
+});
+
+export const fetchData = () => dispatch => {
+  axios
+    .get('http://gabe.mockable.io/quotes')
+    .then(data => {
+      dispatch({ type: SUCCESS, payload: data.data });
+    })
+    .catch(error => {
+      dispatch({ type: FAILURE });
+    });
+};
--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,10 +1,25 @@
-import /* we need our action types here*/ "../actions";
+import * as types from '../actions';
 const initialState = {
-  characters: []
+  characters: [],
+  fetching: false,
+  error: null
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
   switch (action.type) {
+    case types.FETCHING:
+      return {
+        ...state,
+        fetching: true
+      };
+    case types.SUCCESS:
+      return {
+        ...state,
+        fetching: false,
+        characters: action.payload
+      };
+    case types.FAILURE:
+      return state;
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
